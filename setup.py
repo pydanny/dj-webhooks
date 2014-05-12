8208fdb83ad44611c840c39c63a04d719bdfae15
Daniel Greenfeld <pydanny@gmail.com>
2014-05-12 08:51:57 -0700

    Cleaned up setup

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
+        "webhooks",
     ],
     license="BSD",
     zip_safe=False,
@@ -46,10 +47,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
